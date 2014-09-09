481fe1ea3f6ea41965c08196d3da8e825e138382
Tim Chilvers <tim@futureworkshops.com>
2014-09-09 10:07:20 +0100

    Add drag delegate methods

--- Pod/Classes/FWTDragViewDelegate.h
+++ Pod/Classes/FWTDragViewDelegate.h
@@ -8,6 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
+
+@class FWTDragView;
+
 @protocol FWTDragViewDelegate <NSObject>
 
+@optional
+- (void)dragViewWillBeginDragging:(FWTDragView *)dragView;
+- (void)dragViewDidBeginDragging:(FWTDragView *)dragView;
+- (void)dragViewWillEndDragging:(FWTDragView *)dragView;
+- (void)dragViewDidEndDragging:(FWTDragView *)dragView;
 @end
