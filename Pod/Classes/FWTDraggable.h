3f5e4226c1c67376160ec9003a18713100443c3e
Tim Chilvers <tim@futureworkshops.com>
2014-09-15 14:30:53 +0100

    Fix FWTDraggable being in the wrong folder

---
+++ Pod/Classes/FWTDraggable.h
@@ -0,0 +1,39 @@
+//
+//  FWTDraggable.h
+//  FWTDragView
+//
+//  Created by Tim Chilvers on 15/09/2014.
+//  Copyright (c) 2014 Tim Chilvers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol FWTDragViewDelegate;
+
+@protocol FWTDraggable <NSObject>
+
+@required
+
+- (CGPoint)draggedTo;
+- (void)setDraggedTo:(CGPoint)draggedTo;
+
+- (NSArray *)dismissCriteria;
+
+- (NSTimeInterval) dismissAnimationDuration;
+- (void)setDismissAnimationDuration:(NSTimeInterval)dismissAnimationDuration;
+
+- (NSTimeInterval) centerAnimationDuration;
+- (void)setCenterAnimationDuration:(NSTimeInterval)centerAnimationDuration;
+
+- (id <FWTDragViewDelegate>) dragDelegate;
+
+- (CGPoint)lastTouchPoint;
+- (void)setLastTouchPoint:(CGPoint)lastTouchPoint;
+
+- (CGPoint)currentTouchPoint;
+- (void)setCurrentTouchPoint:(CGPoint)currentTouchPoint;
+
+- (CGPoint)initialTouchPoint;
+- (void)setInitialTouchPoint:(CGPoint)initialTouchPoint;
+
+@end
